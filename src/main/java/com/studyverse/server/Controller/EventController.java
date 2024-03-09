0da33c57071f6a2e6e3ac2b3f7959fb16ad50853
NguyenHuuDanh <113421911+DanhNguyen02@users.noreply.github.com>
2024-03-09 15:44:35 +0700

    Create all Event class

--- src/main/java/com/studyverse/server/Controller/EventController.java
+++ src/main/java/com/studyverse/server/Controller/EventController.java
@@ -1,2 +1,28 @@
-package com.studyverse.server.Controller;public class EventController {
+package com.studyverse.server.Controller;
+
+import com.studyverse.server.DAO.EventDAO;
+import com.studyverse.server.Model.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/event")
+public class EventController {
+    @Autowired
+    private EventDAO eventDAO;
+    @GetMapping("/events")
+    public List<Event> getEvents() {
+        return eventDAO.getEvents();
+    }
+
+    @PostMapping("/createEvent")
+    public Map<String, String> createEvent(@RequestBody HashMap<String, String> body) {
+        Map<String, String> response = new HashMap<>();
+
+        return response;
+    }
 }
