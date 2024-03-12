4e7b1b84899c4d3a2cf20c13d9cde0f764e12065
NguyenHuuDanh <113421911+DanhNguyen02@users.noreply.github.com>
2024-03-12 09:42:00 +0700

    Get database variables in application.propeties

--- src/main/java/com/studyverse/server/Service/JdbcConfig.java
+++ src/main/java/com/studyverse/server/Service/JdbcConfig.java
@@ -1,21 +1,36 @@
 package com.studyverse.server.Service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
 import javax.sql.DataSource;
 
 @Configuration
+@Component
 public class JdbcConfig {
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
     @Bean
     public DataSource dataSource() {
         return DataSourceBuilder
                 .create()
-                .url("jdbc:mysql://localhost:3306/server")
-                .username("root")
-                .password("123456")
-                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url(databaseUrl)
+                .username(username)
+                .password(password)
+                .driverClassName(driverClassName)
                 .build();
     }
 
